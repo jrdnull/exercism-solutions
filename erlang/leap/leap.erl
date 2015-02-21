149dd02a53a7b9ac477319906eb98b215d066083
Jordon Smith <jrd@mockra.net>
2015-02-21 11:55:01 +0000

    Refactor erlang leap year based on nitpick

--- erlang/leap/leap.erl
+++ erlang/leap/leap.erl
@@ -2,8 +2,9 @@
 -export([leap_year/1]).
 
 leap_year(Year) ->
-  evenly_divisible(Year, 4) andalso
-    not evenly_divisible(Year, 100) orelse evenly_divisible(Year, 400).
+  evenly_divisible(Year, 4)
+    andalso not evenly_divisible(Year, 100)
+      orelse evenly_divisible(Year, 400).
 
 evenly_divisible(X, Y) ->
   X rem Y == 0.
